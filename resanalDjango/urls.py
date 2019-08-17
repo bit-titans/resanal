7070fa8e99e7b93c927842abbc82d172bcbe6719
Akash <rottiakash@gmail.com>
2019-08-17 16:56:01 +0530

    Fix URL.py

--- resanalDjango/urls.py
+++ resanalDjango/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     url(r'^getfcd/',views.GetFCD.as_view()),
     url(r'^secfcd/',views.FCD_Section.as_view()),
     url(r'^totalfcd/',views.TotalFCD.as_view()),
-    url(r'^genXL/',views.GenXL.as_view())
+    url(r'^genXL/',views.GenXL.as_view()),
+    url(r'^genXLDash/',views.genXLDash.as_view())
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
