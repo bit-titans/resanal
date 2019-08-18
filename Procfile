0490b921fb87df53b5e84d1fc38ef6dc2254ae79
Akash <rottiakash@gmail.com>
2019-08-18 22:43:55 +0530

    Added Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn resanal.wsgi
--- resanalDjango/settings.py
+++ resanalDjango/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = ['vkhand.pythonanywhere.com','127.0.0.1','rottiakash.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','resnalbackend.herokuapp.com/']
 
 
 # Application definition
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 # }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
@@ -132,5 +133,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
